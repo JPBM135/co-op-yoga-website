29960536c0f37a9b96f2f4c0c8a9e8ed6d1c07a7
JPBM135 <jpedrobm0@gmail.com>
2024-02-08 20:36:51 -0800

    feat: other stuff

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import styles from "./header.module.css";
 
 export default function Header() {
+  const [colorScheme, setColorScheme] = useState("light");
+  const [isMounted, setIsMounted] = useState(false);
+
+  const toggleColorScheme = () => {
+    setColorScheme(colorScheme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    console.log("mounted");
+    document.documentElement.setAttribute("data-theme", colorScheme);
+  }, [colorScheme]);
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>Welcome to Next.js!</h1>
+      <button onClick={toggleColorScheme}>Toggle color scheme</button>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import { SectionTwo } from "./components/SectionTwo/SectionTwo";
 import { SectionThree } from "./components/SectionThree/SectionThree";
 import { SectionFour } from "./components/SectionFour/SectionFour";
 import { SectionFive } from "./components/SectionFive/SectionFive";
+import Image from "next/image";
+import home_yoga from "./home-yoga.png";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image src={home_yoga} alt="Picture of the author" />
       <SectionOne />
       <SectionTwo />
       <SectionThree />
