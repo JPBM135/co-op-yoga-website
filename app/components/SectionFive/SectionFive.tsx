cf9300144ffb9126a006f8df4deb87e7f13c2821
JPBM135 <jpedrobm0@gmail.com>
2024-02-11 21:18:43 -0800

    feat(sections): join our newsletter

--- app/components/SectionFive/SectionFive.tsx
+++ app/components/SectionFive/SectionFive.tsx
@@ -1,7 +1,39 @@
+import { ArrowRight } from "@phosphor-icons/react";
+import style from "./sectionFive.module.css";
+
 export function SectionFive() {
+  const clearInput = () => {
+    const input = document.getElementById(
+      "section_five_input"
+    ) as HTMLInputElement | null;
+
+    if (!input) {
+      return;
+    }
+
+    input.value = "";
+  };
+
   return (
-    <section>
-      <h2>Section Five</h2>
+    <section className={style.section}>
+      <h2 className={` ${style.section__title} utils__font-lora`}>
+        Join our newsletter
+      </h2>
+      <p className={style.section__text}>
+        Subscribe to our newsletter to be aware of many things on discounts,
+        gifts, training and much more. Just enter your email.
+      </p>
+      <div className={style.section__container}>
+        <input
+          id="section_five_input"
+          className={style.section__input}
+          type="email"
+          placeholder="For latest update send mail"
+        />
+        <button className={style.section__btn} onClick={clearInput}>
+          Subscribe <ArrowRight size={24} />
+        </button>
+      </div>
     </section>
   );
 }
