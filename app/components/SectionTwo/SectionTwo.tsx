6065765fa4024d41d755244f7b1a7b198f4f5510
JPBM135 <jpedrobm0@gmail.com>
2024-02-13 11:24:42 -0800

    feat(section2): overlay and responsivity

--- app/components/SectionTwo/SectionTwo.tsx
+++ app/components/SectionTwo/SectionTwo.tsx
@@ -1,25 +1,42 @@
 import Image from "next/image";
 import style from "./sectionTwo.module.css";
 import health_fitness from "./health_fitness.png";
-import { ArrowRight } from "@phosphor-icons/react";
+import { ArrowRight, Heart } from "@phosphor-icons/react";
+import { useIsScreenWide } from "../../utils/isScreenWideHook";
+
 export function SectionTwo() {
+  const isScreenWide = useIsScreenWide();
+
   return (
     <section className={style.section}>
-
       <div className={style.image}>
-        <Image src={health_fitness} alt="health and fitness" />
+        <div className={style.image__heart}>
+          <Heart className={style.image__heart__icon} size={32} weight="fill" />
+          <div>
+            <h3 className={style.image__heart__title}>Heart Rate</h3>
+            <p className={style.image__heart__text}>168 bpm</p>
+          </div>
+        </div>
+        <div className={style.image__free}>
+          <h3 className={style.image__free__title}>+500</h3>
+          <p className={style.image__free__text}>Free courses</p>
+        </div>
+        <Image
+          src={health_fitness}
+          alt="health and fitness"
+          width={isScreenWide ? 500 : 320}
+        />
       </div>
 
       <div className={style.container}>
-        <h1 className={style.title}>
-        Bring Happines to Good Health
-        </h1>
+        <h1 className={style.title}>Bring Happiness To Good Health!</h1>
         <p className={style.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, provident? Est quis necessitatibus molestias unde omnis aliquid eum nulla nemo voluptate repellat? Nihil ut facilis corrupti nisi nemo deserunt ipsa.
+          If you take care of your good health, take care of your mentality and
+          lead a healthy life with positive thoughts every day.
         </p>
-          <button className={style.button}>
-           Lets Start<ArrowRight size={24} />
-          </button>
+        <button className={style.button}>
+          Lets start <ArrowRight size={24} />
+        </button>
       </div>
     </section>
   );
