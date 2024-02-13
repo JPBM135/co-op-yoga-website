b1d738d1cfaedea49e1425d0608d02bc6bcdb460
hyemin-project <jhmin07@gmail.com>
2024-02-12 18:00:34 -0800

    section 3,4

--- app/components/SectionFour/SectionFour.tsx
+++ app/components/SectionFour/SectionFour.tsx
@@ -1,4 +1,4 @@
-/*use client*/
+"use client";
 
 import Image from "next/image";
 import styles from "./sectionFour.module.css"
@@ -7,85 +7,70 @@ import yoga2 from "./follow-yoga-2.png";
 import yoga3 from "./follow-yoga-3.png";
 import yoga4 from "./follow-yoga-4.png";
 import { useState, useRef, useEffect } from "react";
+import { useIsScreenWide } from "../../utils/isScreenWideHook";
+import { safeWindow } from "../../utils/safeWindow";
+import { useIsElementVisible } from "../../utils/isElementVisible";
+
 
 export function SectionFour() {
 
-  // const [isInViewport, setIsInViewport] = useState(false);
-  // const ref = useRef<HTMLDivElement | null>(null);
+  const _window = safeWindow();
+  const element = document.getElementById("section4");
+  const isVisible = useIsElementVisible(element);
 
-  // useEffect(() => {
-  //   if (!ref.current) return; 
+  let  returnContext;
 
-  //   const callback = (entries: IntersectionObserverEntry[]) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         // appeared 
-  //         setIsInViewport(true);
-  //       } else {
-  //         // disappeard
-  //         setIsInViewport(false);
-  //       }
-  //     });
-  //   };
+ const isMobile = useIsScreenWide();
 
-  //   const options = { root: null, rootMargin: "0px", threshold: 0 };
-
-  //   const observer = new IntersectionObserver(callback, options);
-  //   observer.observe(ref.current); //Start observation
-
-  //   return () => {
-  //     observer.disconnect(); //Stop observation
-  //   };
-  // }, []);
-
-
-  // return (
-  //   <section>
-  //     <div className={styles.flex}>
-  //       <div className={styles.title}>
-  //         <h2>Follow Us On Instagram</h2>
-  //         <h2 className={styles.gradient}>#YOGA</h2>
-  //       </div>
-  //       <div className={styles.button}>
-  //         <p>Follow</p><p>&#10132;</p>
-  //       </div>
-
-  //       <div className={styles.image_part}>
-  //           <div className={styles.image_top_div}>
-  //           <Image src={yoga1} alt={"yoga1"} className={scrollPosition >100 ? styles.scroll_yoga1 : ""}></Image>
-            
-  //           <Image src={yoga2} alt={"yoga2"} className={scrollPosition >100 ?styles.scroll_yoga2: ""}></Image>
-  //         </div>
-  //         <Image src={yoga3} alt={"yoga3"} className={scrollPosition  >100?styles.scroll_yoga3: ""}></Image>
-  //         <Image src={yoga4} alt={"yoga4"} className={scrollPosition >100 ?styles.scroll_yoga4: ""}></Image>
-  //       </div>
-
-  //     </div>
-  //   </section>
-  // );
-
-
-  return (
-    <section>
-      <div className={styles.flex}>
-        <div className={styles.title}>
-          <h2>Follow Us On Instagram</h2>
-          <h2 className={styles.gradient}>#YOGA</h2>
-        </div>
-        <div className={styles.button}>
-          <p>Follow</p><p>&#10132;</p>
-        </div>
-
-        <div className={styles.image_part}>
-          <div className={styles.image_top_div}>
-            <Image src={yoga1} alt={"yoga1"} className={styles.scroll_yoga1}></Image>
-            <Image src={yoga2} alt={"yoga2"} className={styles.scroll_yoga2}></Image>
+  if(isMobile){
+    returnContext = (
+      <section id="section4" className={styles.section}>
+        <div className={styles.flex}>
+          <div className={styles.text_part}>
+          <Image src={yoga1} alt={"yoga1"} className={styles.scroll_yoga1}></Image>
+            <div className={styles.title}>
+              <h2>Follow Us On <br></br>Instagram</h2>
+              <h2 className={styles.gradient}>#YOGA</h2>
+              <div className={styles.button}>
+                <p>Follow</p><p>&#10132;</p>
+              </div>
+            </div>
+            <Image src={yoga2} alt={"yoga2"} className={`${styles.scroll_yoga2} ${isVisible ? styles["scroll_yoga2"] : ""}`}></Image>
           </div>
-          <Image src={yoga3} alt={"yoga3"} className={styles.scroll_yoga3}></Image>
-          <Image src={yoga4} alt={"yoga4"} className={styles.scroll_yoga4}></Image>
-        </div>
+          <div className={styles.image_part}>
+            <Image src={yoga3} alt={"yoga3"} className={styles.scroll_yoga3}></Image>
+            <Image src={yoga4} alt={"yoga4"} className={styles.scroll_yoga4}></Image>
+          </div>
 
-      </div>
+        </div>
     </section>
-  );
+    );
+  }else{
+    returnContext = (
+      <section id="section4">
+        <div className={styles.flex}>
+          <div className={styles.text_part}>
+            <div className={styles.title}>
+              <h2>Follow Us On Instagram</h2>
+              <h2 className={styles.gradient}>#YOGA</h2>
+            </div>
+            <div className={styles.button}>
+              <p>Follow</p><p>&#10132;</p>
+            </div>
+          </div>
+          <div className={styles.image_part}>
+            <div className={styles.image_top_div}>
+              <Image src={yoga1} alt={"yoga1"} className={styles.scroll_yoga1}></Image>
+              <Image src={yoga2} alt={"yoga2"} className={`${styles.scroll_yoga2} ${isVisible ? styles["scroll_yoga2"] : ""}`}></Image>
+            </div>
+            <Image src={yoga3} alt={"yoga3"} className={styles.scroll_yoga3}></Image>
+            <Image src={yoga4} alt={"yoga4"} className={styles.scroll_yoga4}></Image>
+          </div>
+
+        </div>
+    </section>
+    );
+  }
+
+  return returnContext;
 }
--- app/components/SectionThree/SectionThree.tsx
+++ app/components/SectionThree/SectionThree.tsx
@@ -1,34 +1,42 @@
 import Image from "next/image";
 import yoga1 from "./routine-yoga-1.png";
 import yoga2 from "./routine-yoga-2.png";
-import box1 from "./Box routine 1.png";
-import box2 from "./Box routine 2.png";
+import box1 from "./ri_play-circle-fill.png";
+import box2 from "./ri_run-fill.png";
+// import box1 from "./Box routine 1.png";
+// import box2 from "./Box routine 2.png";
 import styles from "./sectionThree.module.css";
 
 export function SectionThree() {
   return (
-    <section>
+    <section className={styles.section}>
       <div className={styles.flex}>
-
+        <div className={styles.text_part}>
         <h2 className={styles.title}>Anytime, Any Place, Any Workout</h2>
-        <p className={styles.content}>
-          Take your routine at any time of your day, with the new videos and with the teachers who will guide you.
-        </p>
-        <div className={styles.button}>
-          <span>Get Started Free &#10132;</span>
+          <p className={styles.content}>
+            Take your routine at any time of your day, with the new videos and with the teachers who will guide you.
+          </p>
+          <div className={styles.button}>
+            <span>Get Started Free &#10132;</span>
+          </div>
         </div>
+        
       
       <div className={styles.image_part}>
         <div className={styles.top_image}>
           <div className={styles.image_div1}>
-            <Image src={yoga1} width={250} height={171.88} alt={"routine-yoga-1"}></Image>
-            <Image src={box1} alt={"box1"}  className={styles.box1}></Image>
+            <Image src={yoga1} className={styles.image1} alt={"routine-yoga-1"}></Image>
+            <div className={styles.box1}>
+              <Image src={box1} alt={"box1"}></Image><span>High Quality Video</span>
+            </div>
           </div>
         </div>
         <div className={styles.bottom_image}>
           <div className={styles.image_div2}>
-            <Image src={yoga2} width={250} height={171.88} alt={"routine-yoga-2"} className={styles.image2}></Image>
-            <Image src={box2} alt={"box2"} className={styles.box2}></Image>
+            <Image src={yoga2} alt={"routine-yoga-2"} className={styles.image2}></Image>
+            <div className={styles.box2}>
+              <Image src={box2} alt={"box2"}></Image><span>Professional Trainer</span>
+            </div>
           </div>
         </div>
       </div>
