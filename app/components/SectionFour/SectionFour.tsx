03d5191fd99aacbc2242c66d9067e42dbd839414
JPBM135 <jpedrobm0@gmail.com>
2024-02-12 18:16:20 -0800

    chore: format

--- app/components/SectionFour/SectionFour.tsx
+++ app/components/SectionFour/SectionFour.tsx
@@ -1,51 +1,68 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./sectionFour.module.css"
+import styles from "./sectionFour.module.css";
 import yoga1 from "./follow-yoga-1.png";
 import yoga2 from "./follow-yoga-2.png";
 import yoga3 from "./follow-yoga-3.png";
 import yoga4 from "./follow-yoga-4.png";
-import { useState, useRef, useEffect } from "react";
 import { useIsScreenWide } from "../../utils/isScreenWideHook";
 import { safeWindow } from "../../utils/safeWindow";
 import { useIsElementVisible } from "../../utils/isElementVisible";
 
-
 export function SectionFour() {
-
   const _window = safeWindow();
   const element = _window?.document.getElementById("section4");
   const isVisible = useIsElementVisible(element!);
 
-  let  returnContext;
+  let returnContext;
 
- const isMobile = useIsScreenWide();
+  const isMobile = useIsScreenWide();
 
-  if(isMobile){
+  if (isMobile) {
     returnContext = (
       <section id="section4" className={styles.section}>
         <div className={styles.flex}>
           <div className={styles.text_part}>
-          <Image src={yoga1} alt={"yoga1"} className={styles.scroll_yoga1}></Image>
+            <Image
+              src={yoga1}
+              alt={"yoga1"}
+              className={styles.scroll_yoga1}
+            ></Image>
             <div className={styles.title}>
-              <h2>Follow Us On <br></br>Instagram</h2>
+              <h2>
+                Follow Us On <br></br>Instagram
+              </h2>
               <h2 className={styles.gradient}>#YOGA</h2>
               <div className={styles.button}>
-                <p>Follow</p><p>&#10132;</p>
+                <p>Follow</p>
+                <p>&#10132;</p>
               </div>
             </div>
-            <Image src={yoga2} alt={"yoga2"} className={`${styles.scroll_yoga2} ${isVisible ? styles["scroll_yoga2"] : ""}`}></Image>
+            <Image
+              src={yoga2}
+              alt={"yoga2"}
+              className={`${styles.scroll_yoga2} ${
+                isVisible ? styles["scroll_yoga2"] : ""
+              }`}
+            ></Image>
           </div>
           <div className={styles.image_part}>
-            <Image src={yoga3} alt={"yoga3"} className={styles.scroll_yoga3}></Image>
-            <Image src={yoga4} alt={"yoga4"} className={styles.scroll_yoga4}></Image>
+            <Image
+              src={yoga3}
+              alt={"yoga3"}
+              className={styles.scroll_yoga3}
+            ></Image>
+            <Image
+              src={yoga4}
+              alt={"yoga4"}
+              className={styles.scroll_yoga4}
+            ></Image>
           </div>
-
         </div>
-    </section>
+      </section>
     );
-  }else{
+  } else {
     returnContext = (
       <section id="section4">
         <div className={styles.flex}>
@@ -55,20 +72,38 @@ export function SectionFour() {
               <h2 className={styles.gradient}>#YOGA</h2>
             </div>
             <div className={styles.button}>
-              <p>Follow</p><p>&#10132;</p>
+              <p>Follow</p>
+              <p>&#10132;</p>
             </div>
           </div>
           <div className={styles.image_part}>
             <div className={styles.image_top_div}>
-              <Image src={yoga1} alt={"yoga1"} className={styles.scroll_yoga1}></Image>
-              <Image src={yoga2} alt={"yoga2"} className={`${styles.scroll_yoga2} ${isVisible ? styles["scroll_yoga2"] : ""}`}></Image>
+              <Image
+                src={yoga1}
+                alt={"yoga1"}
+                className={styles.scroll_yoga1}
+              ></Image>
+              <Image
+                src={yoga2}
+                alt={"yoga2"}
+                className={`${styles.scroll_yoga2} ${
+                  isVisible ? styles["scroll_yoga2"] : ""
+                }`}
+              ></Image>
             </div>
-            <Image src={yoga3} alt={"yoga3"} className={styles.scroll_yoga3}></Image>
-            <Image src={yoga4} alt={"yoga4"} className={styles.scroll_yoga4}></Image>
+            <Image
+              src={yoga3}
+              alt={"yoga3"}
+              className={styles.scroll_yoga3}
+            ></Image>
+            <Image
+              src={yoga4}
+              alt={"yoga4"}
+              className={styles.scroll_yoga4}
+            ></Image>
           </div>
-
         </div>
-    </section>
+      </section>
     );
   }
 
--- app/components/SectionThree/SectionThree.tsx
+++ app/components/SectionThree/SectionThree.tsx
@@ -12,35 +12,44 @@ export function SectionThree() {
     <section className={styles.section}>
       <div className={styles.flex}>
         <div className={styles.text_part}>
-        <h2 className={styles.title}>Anytime, Any Place, Any Workout</h2>
+          <h2 className={styles.title}>Anytime, Any Place, Any Workout</h2>
           <p className={styles.content}>
-            Take your routine at any time of your day, with the new videos and with the teachers who will guide you.
+            Take your routine at any time of your day, with the new videos and
+            with the teachers who will guide you.
           </p>
           <div className={styles.button}>
             <span>Get Started Free &#10132;</span>
           </div>
         </div>
-        
-      
-      <div className={styles.image_part}>
-        <div className={styles.top_image}>
-          <div className={styles.image_div1}>
-            <Image src={yoga1} className={styles.image1} alt={"routine-yoga-1"}></Image>
-            <div className={styles.box1}>
-              <Image src={box1} alt={"box1"}></Image><span>High Quality Video</span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.bottom_image}>
-          <div className={styles.image_div2}>
-            <Image src={yoga2} alt={"routine-yoga-2"} className={styles.image2}></Image>
-            <div className={styles.box2}>
-              <Image src={box2} alt={"box2"}></Image><span>Professional Trainer</span>
-            </div>
-          </div>
-        </div>
-      </div>
 
+        <div className={styles.image_part}>
+          <div className={styles.top_image}>
+            <div className={styles.image_div1}>
+              <Image
+                src={yoga1}
+                className={styles.image1}
+                alt={"routine-yoga-1"}
+              ></Image>
+              <div className={styles.box1}>
+                <Image src={box1} alt={"box1"}></Image>
+                <span>High Quality Video</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.bottom_image}>
+            <div className={styles.image_div2}>
+              <Image
+                src={yoga2}
+                alt={"routine-yoga-2"}
+                className={styles.image2}
+              ></Image>
+              <div className={styles.box2}>
+                <Image src={box2} alt={"box2"}></Image>
+                <span>Professional Trainer</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
